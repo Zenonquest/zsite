2ec44d50a65298b9bb89f1546efb7038808d7c70
Zenonquest <Zenonquest@gmail.com>
2016-07-17 16:28:08 -0600

    old project-root

--- zsite/settings.py
+++ zsite/settings.py
@@ -155,7 +155,7 @@ CORS_ALLOW_CREDENTIALS = True
 # import os
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
