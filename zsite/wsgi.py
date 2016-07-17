c26d3c0c3355c6771f50ade89ee3a2470fd32b53
Zenonquest <Zenonquest@gmail.com>
2016-07-17 15:05:43 -0600

    whitenoise from heroku doc

--- zsite/wsgi.py
+++ zsite/wsgi.py
@@ -19,3 +19,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zsite.settings")
 application = get_wsgi_application()
 # application = DjangoWhiteNoise(application)
 application = Cling(get_wsgi_application())
+
+##django-app-configuration (heroku doc)
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
