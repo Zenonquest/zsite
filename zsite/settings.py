1cdadbeb2cb996d57ad3562897a8229bae807e80
Zenonquest <Zenonquest@gmail.com>
2016-07-17 18:26:03 -0600

    removed debugging

--- zsite/settings.py
+++ zsite/settings.py
@@ -18,7 +18,6 @@ import pdb
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-pdb.set_trace()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -168,7 +167,6 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-pdb.set_trace()
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
