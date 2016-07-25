6adbfec931250bbcef79efcca514a75663c03a64
Zenonquest <Zenonquest@gmail.com>
2016-07-24 21:24:09 -0600

    cleanup settings

--- zsite/settings.py
+++ zsite/settings.py
@@ -12,15 +12,10 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 import os
 import dj_database_url
-# from config import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# # PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -43,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #check if necessary
-    'django.contrib.sites',
+    # 'django.contrib.sites',
     'rest_framework',
     'polls',
     'scraper',
@@ -142,62 +137,3 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
-# # Update database configuration with $DATABASE_URL.
-# # DATABASES['default'] = dj_database_url.config()
-
-
-# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# # Allow all host headers
-# ALLOWED_HOSTS = ['*']
-
-# # DEBUG = False
-
-# # try:
-# #     from .local_settings import *
-# # except ImportError:
-# #     pass
-
-
-# # # Static files (CSS, JavaScript, Images)
-# # # https://docs.djangoproject.com/en/1.9/howto/static-files/
-# # # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-# # # STATIC_URL = '/static/'
-
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-# # STATIC_URL = '/static/'
-
-# # # Extra places for collectstatic to find static files.
-# # STATICFILES_DIRS = [
-# #     os.path.join(PROJECT_ROOT, 'static'),
-# # ]
-
-# # # Simplified static file serving.
-# # # https://warehouse.python.org/project/whitenoise/
-# # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-
-# CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ALLOW_CREDENTIALS = True
-
-# # import os
-# # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# # STATIC_ROOT = 'staticfiles'
-# STATIC_URL = '/static/'
-
-# # STATICFILES_DIRS = (
-# #     os.path.join(BASE_DIR, 'static'),
-# # )
-
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
-
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
