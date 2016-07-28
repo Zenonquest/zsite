cdab8df9a8bdaccb6d99da2510bebebeb9c3cea9
Zenonquest <Zenonquest@gmail.com>
2016-07-28 08:30:21 -0600

    fixes to polls

--- polls/views.py
+++ polls/views.py
@@ -17,7 +17,7 @@ class IndexView(generic.ListView):
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class QuestionView(generic.ListView):
-    template_name = 'polls/questions.html'
+    template_name = 'polls/question.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
@@ -26,14 +26,14 @@ class QuestionView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Question
-    template_name = 'polls/detail.html'
+    template_name = 'polls/details.html'
     def get_queryset(self):
     	return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'polls/results.html'
+    template_name = 'polls/result.html'
 
 def vote(request, question_id):
 	p=get_object_or_404(Question, pk=question_id)
@@ -41,7 +41,7 @@ def vote(request, question_id):
 		selected_choice = p.choice_set.get(pk=request.POST['choice'])
 	except (KeyError, Choice.DoesNotExist):
 		#Redisplay the question voting form.
-		return render(request, 'polls/detail.html',{
+		return render(request, 'polls/details.html',{
 			'question': p,
 			'error_message': "You didn't select a choice.",
 			})
